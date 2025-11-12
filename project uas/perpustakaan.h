5704e8aad5cb1efb78cbf64d64d7b520124ebb11
rafiealghifari987-cpu <rafiealghifari987@gmail.com>
2025-11-12 08:21:08 +0700

    Update perpustakaan.h

--- project uas/perpustakaan.h
+++ project uas/perpustakaan.h
@@ -23,17 +23,13 @@
 
 typedef struct{
    int id;
-
    int judul [MAX_STR];
-
    int penulis [MAX_STR];
-
    int kategori [MAX_STR];
-
    int tersedia;
-
    int dipinjamCount;   
-}buku;
+
+}Buku;
 
 
 
@@ -41,12 +37,55 @@ typedef struct{
 typedef struct{
 
    int id;
-
    char nama [MAX_STR];
-
    char alamat [MAX STR];
 
-}anggota;
+}Anggota;
+
+
+typedef struct {
+   int id ;
+   int idBuku;
+   int idAnggota;
+   int tanggalPinjam[20];
+   char tanggalKembali[20];
+   int denda;
+
+}Peminjaman;
+
+
+void tampilkanMenuUtama() ;
+void loginPustakawan() ;
+
+void tambahanBuku();
+void hapusBuku() ;
+void editBuku() ;
+void tampilkanDaftarBuku() ;
+void cariBuku() ;
+void rangkingBuku() ;
+
+void tambahAnggota() ;
+void tampilkanAnggota() ;
+
+void pinjamBuku() ;
+void kembalikanBuku() ;
+
+int hitungDenda(const char *tlgPinjam, const char *tglKembali) ;
+void simpanBuku(Buku buku) ;
+void simpanAnggota(Anggota anggota);
+void simpanPeminjaman(Peminjaman peminjam) ;
+
+void bacaBukuDariFile(Buku daftarBuku[], int *jumlah) ;
+void bacaAnggotaDariFile(Anggota daftarAnggota[], int *jumlah) ;
+voidbacaPeminjamDariFile(Peminjaman daftarPeminjam[], int *jumlah) ;
+
+#endif
+
+
+
+
+
+
 
 
 
