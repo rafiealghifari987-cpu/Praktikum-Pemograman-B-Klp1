e028bc70aa8130bb1ba566c8ace15d6301381b0b
rafiealghifari987-cpu <rafiealghifari987@gmail.com>
2025-11-12 08:42:54 +0700

    Update main.c

--- project uas/main.c
+++ project uas/main.c
@@ -1,1 +1,37 @@
-#include <stdio.h>
+#include "perpustakaan.h"
+
+int main() {
+    
+    loginPustakawan() ;
+    tampilkanMenuUtama() ;
+    return 0;
+}
+
+void loginPustakawan() {
+     char user [50], pass[50]
+     do {
+         printf("=== LOGIN PUSTAKAWAN             
+         ===\n");
+         printf("Username: ") ;
+         scanf("%s", user) ;
+         printf("Password: ") ;
+         scanf("%s", pass) ;
+         if 
+
+
+
+
+
+
+      }
+
+
+
+
+
+
+
+
+
+
+}
