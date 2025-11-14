d59b1394a831bc050cdc187b677e05de1f461167
rafiealghifari987-cpu <rafiealghifari987@gmail.com>
2025-11-14 08:57:20 +0700

    Update main.c

--- project uas/main.c
+++ project uas/main.c
@@ -1,29 +1,28 @@
+#include <stdio.h>
+#include <string.h>
 #include "perpustakaan.h"
 
 int main() {
-    
-    loginPustakawan() ;
-    tampilkanMenuUtama() ;
+    loginPustakawan();
+    tampilkanMenuUtama();
     return 0;
 }
 
 void loginPustakawan() {
-     char user [50], pass[50];
-     do {
-         printf("=== LOGIN PUSTAKAWAN ===\n");    
-         printf("Username: ") ;
-         scanf("%s", user) ;
-         printf("Password: ") ;
-         scanf("%s", pass) ;
-         if(strcmp(user, " admin")==0
-         && strcmp(pass, "klp1solid")==0){      
-         printf("\nLogin berhasil!\n\n") ;
-         break;
-         } else{
-              printf("Login gagal! Cobalagi.\n\n") ;   
-              
-         }
-       
-     }while(1);
+    char user[50], pass[50];
+    do {
+        printf("=== LOGIN PUSTAKAWAN ===\n");
+        printf("Username: ");
+        scanf("%s", user);
+        printf("Password: ");
+        scanf("%s", pass);
 
+        if (strcmp(user, "admin") == 0 && strcmp(pass, "klp1solid") == 0) {
+            printf("\nLogin berhasil!\n\n");
+            break;
+        } else {
+            printf("Login gagal! Coba lagi.\n\n");
+        }
+
+    } while (1);
 }
